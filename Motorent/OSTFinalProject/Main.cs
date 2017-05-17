f195c62ac79cbadbb86f496dd17a12f85f8e6af5
Changrs <changrs13@hotmail.com>
2017-05-17 15:00:19 +0800

    Add files via upload

---
+++ Motorent/OSTFinalProject/Main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Modals;
+namespace OSTFinalProject
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        List<Vehicle> VehicleList = new List<Vehicle>();
+
+        private void BtnAddVehicle_Click(object sender, EventArgs e)
+        {
+            AddVehicleForm f1 = new AddVehicleForm();
+            this.Hide();
+            f1.Show();
+        }
+
+        private void BtnAddJourney_Click(object sender, EventArgs e)
+        {
+            RegisterJourneyForm f1 = new RegisterJourneyForm();
+            this.Hide();
+            f1.Show();
+        }
+    }
+}
